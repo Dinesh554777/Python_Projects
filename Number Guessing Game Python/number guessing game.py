ae5ee218425b366e5a197ab25618bd260962a057
Dinesh <dinesh6212193@gmail.com>
2026-04-22 20:19:24 +0530

    update the python program

--- Number Guessing Game Python/number guessing game.py
+++ Number Guessing Game Python/number guessing game.py
@@ -2,7 +2,7 @@ def number_guessing_game():
     import random
 
     print("Welcome to Guessing Number Game:)")
-    print("Please enter a number between 1 to 100:")
+    print("Please enter a number between 1 to 100!")
 
     secret_number = random.randint(1, 100)
     attempts = 0
@@ -12,7 +12,7 @@ def number_guessing_game():
             user_input = int(input("Enter the number: "))
             
             if user_input < 1 or user_input > 100:
-                print("Invalid number, please enter between 1 to 100!!!")
+                print("Invalid number, please enter between 1 to 100!")
                 continue
 
             attempts += 1
